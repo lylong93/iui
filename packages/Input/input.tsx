b117cf8e07b247e6b5dc113c0ac300beec56681c
lyl <695498118@qq.com>
2020-08-08 15:54:53 +0800

    inputstyle

--- packages/Input/input.tsx
+++ packages/Input/input.tsx
@@ -1,21 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState,useRef, useEffect } from 'react'
 import style from './style'
 const Input = () => {
+    const inputRef:any= useRef()
     const [state,changeState] = useState(false)
     const {input,inputWrapper,w} = style(state)
-    const foc =() => {
-        changeState(true)
-    }
-    const blu =() => {
-        changeState(false)
-    }
+
+    // const foc =() => {
+    //     changeState(true)
+    // }
+    // const blu =() => {
+    //     changeState(false)
+    // }
     const d = () => {
         changeState(!state)
+        if(state) {
+            inputRef.current.focus()
+        }
     }
+    
     return (
         <div className={inputWrapper} onClick={d}>
             <div className={w} >
-                <input className={input} type="text" placeholder="请输入" />
+                <input  ref={inputRef} className={input}   type="text" placeholder="请输入" />
             </div>
         </div>
     )
