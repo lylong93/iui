f1184ed474f0c055206fd26a7d045465928fba6d
lyl <695498118@qq.com>
2020-08-27 20:42:43 +0800

    style

--- packages/Button/button.tsx
+++ packages/Button/button.tsx
@@ -6,20 +6,32 @@ interface ButtonValue {
     click?:any;
     size?:string
 }
+
+let time  = 6
+//24
+// 6-18 am  0-12
+// 18-24 pm  0-12
 function Button (props:ButtonValue) {
     const [mouseStyle, changeStyle] = React.useState(true)
-    let time = 6
     const {children,click,size}  = props
 
     let style = dd(mouseStyle,time,size)
 
     const mousedown =() => {
+        // console.log('mousedown')
         changeStyle(false)
     }
     const mouseup =() => {
-        click()
+        // console.log('mouseup')
+        // click()
         changeStyle(true)
     }
+    useEffect(()=> {
+        setInterval(()=> {
+        time ++
+        style= dd(mouseStyle,time,size)
+        },1000)
+    })
     
     return (
         <Mzu>
--- unit/change.tsx
+++ unit/change.tsx
@@ -7,12 +7,140 @@ interface LabelledValue {
 const getShadowStyle = (selectorText:string,time:number,state:boolean) => {
     let ytime = Math.abs(time)
     let rotime = -time/3
+    // am 0 上午 6
+    let rule,xl,yl,xr,yr
 
-    let rule
     
+
+    switch (time) {
+        case 6:
+            xl = -2 
+            yl = 0
+            xr = 6
+            yr = 0
+            break;
+        case 7:
+            xl = -2 
+            yl = 0
+            xr = 6
+            yr = 0
+            break;          
+        case 8:
+            xl = -2 
+            yl = 0
+            xr = 6
+            yr = 0
+            break;
+        case 9:
+            xl = -2 
+            yl = 0
+            xr = 6
+            yr = 0
+            break;
+        case 10:
+            xl = -2 
+            yl = 0
+            xr = 6
+            yr = 0
+            break;
+        case 11:
+            xl = -2 
+            yl = 0
+            xr = 6
+            yr = 0
+            break;
+        case 12:
+            xl = 0
+            yl = 0
+            xr = 0
+            yr = 0
+            break;
+        case 13:
+            xl = 0.6
+            yl = -0.6
+            xr = -1
+            yr = 2
+            break;
+        case 14:
+            xl = 0.3 
+            yl = -0.3
+            xr = -2
+            yr = 4
+            break;          
+        case 15:
+            xl = 1
+            yl = -1
+            xr = -3
+            yr = 6
+            break;
+        case 16:
+            xl = 1.3
+            yl = -0.6
+            xr = -4
+            yr = 4
+            break;
+        case 17:            
+            xl = 1.6
+            yl = -0.3
+            xr = -4
+            yr = 2
+            break;
+        case 18:
+            xl = 2
+            yl = 0
+            xr = -6
+            yr = 0
+            break;
+        default:
+            break;
+    }
+
+
+
+    
+    // am 7
+
+
+    // am 6 中午 12
+
+    // xl = 0
+    // yl = 0
+    // xr = 0
+    // yr = 2
+
+
+
+    // // am 9  下午 3
+
+    // xl = 1
+    // yl = -1
+    // xr = -3
+    // yr = 6
+
+    // //4点
+
+    // xl = 1.3
+    // yl = -0.6
+    // xr = -4
+    // yr = 4
+
+    // //5点
+
+    // xl = 1.6
+    // yl = -0.3
+    // xr = -4
+    // yr = 2
+
+    // // 12 下午 6
+
+    // xl = 2
+    // yl = 0
+    // xr = -6
+    // yr = 0
+
     // animation:mymove 2s linear infinite; */
     if(state) {
-        rule = `${rotime}px ${rotime}px 3px #fff,${time}px ${ytime}px 3px grey`
+        rule = `${xl}px ${yl}px 3px #fff,${xr}px ${yr}px 3px grey`
     }else {
         rule = ` inset -0.5px -1px 1px  #fff, inset 1px 1px 3px  grey;`
     }
